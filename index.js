67441be5ad23da04b9b6d0476ffd0cbac33ec491
MBappy-404 <muhammadbappy989@gmail.com>
2022-11-09 00:16:24 +0530

    added reviews collection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -25,6 +25,7 @@ async function run(){
 
      try{
           const serviceCollection = client.db('homeService').collection('services');
+          const reviewCollection =  client.db('homeService').collection('reviews');
 
           app.get('/services', async (req, res)=>{
                const query = {};
@@ -33,6 +34,27 @@ async function run(){
                res.send(services)
 
           } )
+
+          app.get('/allService', async(req, res)=>{
+               const query = {};
+               const cursor = serviceCollection.find(query);
+               const services = await cursor.toArray();
+               res.send(services)
+          });
+
+          app.get('/allService/:id', async(req,res)=>{
+               const id = req.params.id;
+               const query = {_id: ObjectId(id)};
+               const details = await serviceCollection.findOne(query);
+               res.send(details);
+               
+          })
+
+          //Reviews
+          app.post('/reviews', (req, res)=>{
+               const reviews = {name:Bappy, logo:'lskdoidosk'}
+               const result = reviewCollection.insertOne(reviews);
+          })
 
 
      }
