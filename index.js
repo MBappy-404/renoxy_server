ab2d4a1eae6cc103f8b0514ebdd17c8a193f110b
MBappy-404 <muhammadbappy989@gmail.com>
2022-11-09 22:57:15 +0530

    add jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,8 +19,23 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.wss65wz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
 // console.log(uri);
+
+function verifyJWT(req, res, next){
+     const authHeader = req.headers.authorization;
+     if(!authHeader){
+          return res.status(401).send({message: 'unauthorized access'});
+      }
+      const token = authHeader.split(' ')[1];
+  
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+          if(err){
+              return res.status(403).send({message: 'Forbidden access'});
+          }
+          req.decoded = decoded;
+          next();
+      })
+}
 
 async function run(){
 
@@ -27,6 +43,13 @@ async function run(){
           const serviceCollection = client.db('homeService').collection('services');
           const reviewCollection =  client.db('homeService').collection('reviews');
 
+          app.post('/jwt', (req,res)=>{
+               const user = req.body;
+               const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+               res.send({token});
+               console.log(token);
+
+          })
           app.get('/services', async (req, res)=>{
                const query = {};
                const cursor = serviceCollection.find(query);
